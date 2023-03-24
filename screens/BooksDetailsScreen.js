75b050e2d62ba4b60f454b6054c88d48a814ae8b
Kraimps Pierre <pier_lupo@laposte.net>
2023-03-24 11:00:38 +0100

    tp

---
+++ screens/BooksDetailsScreen.js
@@ -0,0 +1,51 @@
+import { StyleSheet, Text, View, ScrollView, Image } from 'react-native'
+import React from 'react'
+// import { BOOKS } from '../data/data';
+import BookDetails from '../components/BookDetails';
+
+
+export default function BooksDetailScreen({ navigation, route }) {
+    const BookId = route.params.bookId;
+    const myBook = BOOKS.find((book) => book.id == bookId)
+
+    return (
+        <ScrollView style={styles.rootContainer}>
+            <Image style={styles.image} source={{ uri: myBook.imageUrl }} />
+            <Text style={styles.title}>{myBook.title}</Text>
+            <BookDetails
+                author={myBook.author}
+                ReleaseDate={myBook.ReleaseDate}
+                category={myBook.category}
+                summary={myBook.summary}
+                textStyle={styles.detailText}
+            />
+        </ScrollView>
+        
+    )
+}
+
+const styles = StyleSheet.create({
+    rootContainer: {
+        marginBottom: 32,
+    },
+    image: {
+        width: '100%',
+        height: 350,
+    },
+    title: {
+        fontWeight: 'bold',
+        fontSize: 24,
+        margin: 8,
+        textAlign: 'center',
+        color: 'black',
+    },
+    detailText: {
+        color: 'white',
+    },
+    listOuterContainer: {
+        alignItems: 'center',
+    },
+    listContainer: {
+        width: '80%',
+    },
+})
--- screens/HomePage.js
+++ screens/HomePage.js
@@ -1,12 +1,36 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { ImageBackground, Pressable, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-export default function HomePage() {
+export default function HomePage({ navigation }) {
   return (
-    <View>
-      <Text>HomePage</Text>
+    <View style={styles.container}>
+
+      <ImageBackground source={require('../assets/bnf-2021-140128md.webp')} resizeMode="cover" style={styles.img}>
+      
+      {/* <Pressable onPress={() => navigation.navigate('Log')}> */}
+      <Text style={styles.title}>Welcome on Read and Share...</Text>
+      {/* </Pressable> */}
+      
+      </ImageBackground>
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+
+  container : {
+    flex:1,
+  },
+  title: {
+    flex: 1,
+    fontSize:50,
+    fontFamily: 'cursive',
+    textAlign: 'center',
+    textAlignVertical: 'center',
+    color: 'white',
+  },
+  img: {
+    flex:1,
+
+  }
+})
