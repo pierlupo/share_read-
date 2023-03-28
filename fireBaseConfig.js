0e6578dccf392eda7de988bc5aad29e8444c4e85
Kraimps Pierre <pier_lupo@laposte.net>
2023-03-28 13:59:30 +0200

    wip

--- fireBaseConfig.js
+++ fireBaseConfig.js
@@ -1,3 +1,15 @@
-const API_KEY = " AIzaSyAQ4b-jUpH1kLWVMB5B_6U3ClvsNhNEjdw ";
-const SIGN_IN_URL ="https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=[AIzaSyAQ4b-jUpH1kLWVMB5B_6U3ClvsNhNEjdw]";
-const SIGN_UP_URL ="https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=[AIzaSyAQ4b-jUpH1kLWVMB5B_6U3ClvsNhNEjdw]";
+export const API_KEY = " AIzaSyAQ4b-jUpH1kLWVMB5B_6U3ClvsNhNEjdw ";
+
+export const SIGN_IN_URL = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key="+API_KEY
+
+export const SIGN_UP_URL = "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key="+API_KEY
+
+//export const BASE_DB_URL
+
+// export const API_KEY = " AIzaSyAD1qsI5ds_-cyqhZ2LuOytEGHDt7FB6iM "
+
+// export const BASE_DB_URL = "https://imc-app-1c898-default-rtdb.europe-west1.firebasedatabase.app/"
+
+// export const SIGN_UP_URL = "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key="+API_KEY
+
+// export const SIGN_IN_URL = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key="+API_KEY
