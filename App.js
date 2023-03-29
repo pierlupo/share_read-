b238362c6761e0b34a503bd07c7e025be9e241fd
Kraimps Pierre <pier_lupo@laposte.net>
2023-03-29 23:21:21 +0200

    wip

--- App.js
+++ App.js
@@ -13,6 +13,7 @@ import {
   Image,
   ImageBackground,
   Dimensions,
+  TouchableOpacity,
 } from 'react-native';
 import HomePage from './screens/HomePage';
 import LogPage from './screens/LogPage';
@@ -28,7 +29,7 @@ import {Provider, useSelector} from 'react-redux';
 import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
 import {faUsers} from '@fortawesome/free-solid-svg-icons/faUsers';
 import ActionBarImage from './ActionBarImage';
-import Icon from 'react-native-vector-icons/FontAwesome5'
+import Icon from 'react-native-vector-icons/FontAwesome5';
 // import { DrawerContent } from './screens/DrawerContent';
 
 const Stack = createNativeStackNavigator();
@@ -36,23 +37,51 @@ const Drawer = createDrawerNavigator();
 
 function CustomDrawerContent(props) {
   return (
-   
-  <View style={{flex:1}}>
-    <ImageBackground style={{flex:1,justifyContent:"space-between",padding:20,backgroundColor:"rgb(0,0,0)",borderBottomWidth:2,}} imageStyle=
-           {{opacity:0.8}}
-          resizeMode="cover"
-          source={require('./assets/books-1655783_960_720.jpg')}>
-      <DrawerContentScrollView style={{flex:1}} {...props}>
-        <DrawerItemList {...props} />
-        <DrawerItem 
-         label="Help"
-          onPress={() => Linking.openURL('https://mywebsite.com/help')}
-        
-        />
-      </DrawerContentScrollView>
+    <View style={{flex: 1}}>
+            <ImageBackground
+        style={{
+          flex: 1,
+          justifyContent: 'space-between',
+          padding: 20,
+          backgroundColor: 'rgb(0,0,0)',
+          borderBottomWidth: 2,
+        }}
+        imageStyle={{opacity: 0.8}}
+        resizeMode="cover"
+        source={require('./assets/books-1655783_960_720.jpg')}>
+              <View>
+            <View
+            style={{
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+              padding: 20,
+              backgroundColor: '#f6f6f6',
+              marginBottom: 20,
+              color: 'white'
+            }}>
+              <Text>John Doe</Text>
+              <Text>email@example.com</Text>
+              <Image
+                source={require('./assets/user.png')}
+                style={{width: 60, height: 60, borderRadius: 30}}
+              />
+            </View>
+          </View>
+
+        <DrawerContentScrollView style={{flex: 1}} {...props}>
+          <DrawerItemList {...props} />
+          <DrawerItem
+            label="Help"
+            onPress={() => Linking.openURL('https://mywebsite.com/help')}
+          />
+  
+        </DrawerContentScrollView>
+        <TouchableOpacity style={{position:"absolute", right: 0, left: 0, bottom: 50, backgroundColor: '#f6f6f6', padding: 20}}>
+        <Text>Log Out</Text>
+        </TouchableOpacity>
       </ImageBackground>
     </View>
-  
   );
 }
 
@@ -74,42 +103,33 @@ function DrawerNavigator() {
         headerTitleAlign: 'center',
         drawerPosition: 'right',
         drawerType: 'back',
-        drawerActiveBackgroundColor: 'grey',
+        //drawerActiveBackgroundColor: 'grey',
         drawerStyle: {
           // backgroundColor: '#c6cbef',
           width: '100%',
           // drawerWidth:  Dimensions.get('window').width / 2.0,
         },
-        
       }}>
       <Drawer.Screen
         name="Home"
         component={HomePage}
-        options={{headerShown: true,
-        // drawerItemStyle: {backgroundColor: 'red'},
-        drawerLabelStyle: {fontSize: 20},
-        drawerLabelStyle: [{textAlign: "center"},]
-  //       drawerIcon: () => (
-  //         <Icon
-  //           source={require('./assets/home.png')}
-  //           style={[styles.iconStyles, { tintColor: 'black' }]}
-  //         /> 
-  // )
-}}
+        options={{
+          headerShown: true,
+          // drawerItemStyle: {backgroundColor: 'red'},
+          //drawerLabelStyle: {fontSize: 20},
+          drawerLabelStyle: {textAlign: 'center'},
+          drawerIcon: ({}) => <Icon source={require('./assets/home.png')} />,
+        }}
       />
       <Drawer.Screen
         name="Categories"
         component={CategoriesScreen}
-        options={{title: 'All Categories',
-        drawerLabelStyle: {fontSize: 20},
-      }}
+        options={{title: 'All Categories', drawerLabelStyle: {fontSize: 20}}}
       />
       <Drawer.Screen
         name="Log"
         component={LogPage}
-        options={{title: 'Register or LogIn',
-        drawerLabelStyle: {fontSize: 20},
-      }}
+        options={{title: 'Register or LogIn', drawerLabelStyle: {fontSize: 20}}}
       />
     </Drawer.Navigator>
   );
@@ -125,7 +145,6 @@ function DrawerNavigator() {
 // }
 
 function App() {
-
   return (
     <Provider store={store}>
       <NavigationContainer>
@@ -169,8 +188,7 @@ const styles = StyleSheet.create({
   iconStyles: {
     width: 24,
     height: 24,
-
-  }
+  },
 });
 
 export default App;
